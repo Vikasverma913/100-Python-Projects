8ea159537d783c4839d445acf9e48d9530fdeba3
Vikas Verma <86831037+Vikasverma913@users.noreply.github.com>
2023-12-04 16:47:51 +0530

    Add files via upload

---
+++ Band Name Generator/Project_1 - Brand Name Generator.py
@@ -0,0 +1,6 @@
+# BAND NAME GENERATOR
+
+print("Hello, User")
+input_1 = input("What is the name of the city you grew up in?\n")
+input_2 = input("What is the name of your pet?\n")
+print("The name of your band is:" + input_1 + " " + input_2)
